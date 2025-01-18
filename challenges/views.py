28339b93de77d34c65a891bdb50e6eb76f1b422f
Jerome <ejikejerome@gmail.com>
2025-01-18 06:20:56 +0100

    added function for int in url

--- challenges/views.py
+++ challenges/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+def monthly_challenge_by_number(request, month):
+    challenge_text= None
+    if month == 1:
+        challenge_text = 'Exercise'
+    return HttpResponse(challenge_text)
 
 def monthly_challenge(request, month):
     challenge_text= None
